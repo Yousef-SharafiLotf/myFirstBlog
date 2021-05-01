8b72cb674dc38bd22f486ab8f57ec28faf847781
Yousef SharfiLotf <yousef.sharafi65@gmail.com>
2021-05-01 11:43:23 +0430

    show all todos

--- myFirstBlog/todo/views.py
+++ myFirstBlog/todo/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from . models import Todo
 
 def sayHello(request):
     return render(request, 'sayHello.html')
+
+def allTodos(request):
+    todos = Todo.objects.all()
+    context = {'todos': todos, 'pageTitle': 'all todos'}
+    return render(request, 'all_todos.html', context)
